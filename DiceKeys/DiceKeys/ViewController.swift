788c4c66ed03c483276d2142677eff6586ea19a0
Nikita Titov <nmtitov@ya.ru>
2020-10-26 13:38:22 +0200

    Create Xcode proj

---
+++ DiceKeys/DiceKeys/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceKeys
+//
+//  Created by Nikita Titov on 26.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
