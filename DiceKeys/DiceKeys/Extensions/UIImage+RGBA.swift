8982e77811e2ae246e11de949c7e78639e9f5211
Nikita Titov <nmtitov@ya.ru>
2020-10-29 12:33:48 +0200

    Update UIImage.rgba implementation

--- DiceKeys/DiceKeys/Extensions/UIImage+RGBA.swift
+++ DiceKeys/DiceKeys/Extensions/UIImage+RGBA.swift
@@ -31,28 +31,33 @@ extension UIImage {
          }
         */
         // Return empty `Data` collection if `UIImage` itself is empty
-        guard let bitmap = self.cgImage?.dataProvider?.data else {
+//        guard let bitmap = self.cgImage?.dataProvider?.data else {
+//            return Data()
+//        }
+//
+//        var bytes = [UInt8]()
+//        var ptr: UnsafePointer<UInt8> = CFDataGetBytePtr(bitmap)
+//
+//        for _ in 0 ..< Int(self.size.height) {
+//            for _ in 0 ..< Int(self.size.width) {
+//                // Read r, g, b, a bytes from binary string
+//                let r = ptr.pointee; ptr = ptr.advanced(by: 1)
+//                let g = ptr.pointee; ptr = ptr.advanced(by: 1)
+//                let b = ptr.pointee; ptr = ptr.advanced(by: 1)
+//                let a = ptr.pointee; ptr = ptr.advanced(by: 1)
+//                // Save values to array
+//                bytes.append(r)
+//                bytes.append(g)
+//                bytes.append(b)
+//                bytes.append(a)
+//            }
+//        }
+//
+//        return Data(bytes)
+        if let cfData = self.cgImage?.dataProvider?.data {
+            return cfData as Data
+        } else {
             return Data()
         }
-
-        var bytes = [UInt8]()
-        var ptr: UnsafePointer<UInt8> = CFDataGetBytePtr(bitmap)
-
-        for _ in 0 ..< Int(self.size.height) {
-            for _ in 0 ..< Int(self.size.width) {
-                // Read r, g, b, a bytes from binary string
-                let r = ptr.pointee; ptr = ptr.advanced(by: 1)
-                let g = ptr.pointee; ptr = ptr.advanced(by: 1)
-                let b = ptr.pointee; ptr = ptr.advanced(by: 1)
-                let a = ptr.pointee; ptr = ptr.advanced(by: 1)
-                // Save values to array
-                bytes.append(r)
-                bytes.append(g)
-                bytes.append(b)
-                bytes.append(a)
-            }
-        }
-
-        return Data(bytes)
     }
 }
