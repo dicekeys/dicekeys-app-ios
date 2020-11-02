4601503a3d5ca0ebe595b05d036edacdb514b0eb
Nikita Titov <nmtitov@ya.ru>
2020-11-02 16:11:49 +0200

    Refactor extensions

--- project_ios/DiceKeys/Extensions/Bitmap.swift
+++ project_ios/DiceKeys/Extensions/Bitmap.swift
@@ -9,32 +9,9 @@ import UIKit
 
 public extension UIImage {
     convenience init?(bitmap: Data, width: Int, height: Int) {
-        let bitsPerComponent = 8
-        let bitsPerPixel = 4 * 8
-        let bytesPerRow = 4 * width
-        let colorSpace = CGColorSpaceCreateDeviceRGB()
-        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
-
-        guard let provider = CGDataProvider(data: bitmap as CFData) else {
+        guard let cgImage = CGImage.make(from: bitmap, width: width, height: height) else {
             return nil
         }
-
-        guard let cgImage = CGImage(
-            width: width,
-            height: height,
-            bitsPerComponent: bitsPerComponent,
-            bitsPerPixel: bitsPerPixel,
-            bytesPerRow: bytesPerRow,
-            space: colorSpace,
-            bitmapInfo: bitmapInfo,
-            provider: provider,
-            decode: nil,
-            shouldInterpolate: true,
-            intent: .defaultIntent
-        ) else {
-            return nil
-        }
-
         self.init(cgImage: cgImage)
     }
 
@@ -67,38 +44,30 @@ public extension CGImage {
         context.draw(self, in: CGRect(x: 0, y: 0, width: width, height: height))
         return Data(buffer)
     }
-}
 
-public extension CGFloat {
-    func toRadians() -> CGFloat {
-        self / (180 * .pi)
-    }
+    static func make(from bitmap: Data, width: Int, height: Int) -> CGImage? {
+        let bitsPerComponent = 8
+        let bitsPerPixel = 4 * 8
+        let bytesPerRow = 4 * width
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
 
-    func toDegrees() -> CGFloat {
-        self * (180 * .pi)
-    }
-}
+        guard let provider = CGDataProvider(data: bitmap as CFData) else {
+            return nil
+        }
 
-public extension UIImage {
-    func rotate(radians: Float) -> UIImage? {
-        var newSize = CGRect(origin: CGPoint.zero, size: size).applying(CGAffineTransform(rotationAngle: CGFloat(radians))).size
-        // Trim off the extremely small float value to prevent core graphics from rounding it up
-        newSize.width = floor(newSize.width)
-        newSize.height = floor(newSize.height)
-
-        UIGraphicsBeginImageContextWithOptions(newSize, false, scale)
-        let context = UIGraphicsGetCurrentContext()!
-
-        // Move origin to middle
-        context.translateBy(x: newSize.width / 2, y: newSize.height / 2)
-        // Rotate around middle
-        context.rotate(by: CGFloat(radians))
-        // Draw the image at its center
-        draw(in: CGRect(x: -size.width / 2, y: -size.height / 2, width: size.width, height: size.height))
-
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-
-        return newImage
+        return CGImage(
+            width: width,
+            height: height,
+            bitsPerComponent: bitsPerComponent,
+            bitsPerPixel: bitsPerPixel,
+            bytesPerRow: bytesPerRow,
+            space: colorSpace,
+            bitmapInfo: bitmapInfo,
+            provider: provider,
+            decode: nil,
+            shouldInterpolate: true,
+            intent: .defaultIntent
+        )
     }
 }
