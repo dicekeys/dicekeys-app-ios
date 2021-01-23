991cafbccdb4dc3043249a34c818242cb19f440a
Stuart Schechter <stuart@dicekeys.com>
2021-01-23 18:46:30 +0900

    rejigger keyboard

--- DiceKeys/Views/Screens/EditableDiceKeyView/DiceKeyboardView.swift
+++ DiceKeys/Views/Screens/EditableDiceKeyView/DiceKeyboardView.swift
@@ -7,47 +7,96 @@
 
 import SwiftUI
 
+private struct KeyboardKey<Content: View>: View {
+    let size: CGSize
+    let action: () -> Void
+    let label: () -> Content
+
+    var body: some View {
+        Button(action: action, label: {
+            label()
+//            .aspectRatio(2, contentMode: .fit)
+            .frame(width: size.width * 0.95, height: size.height * 0.95)
+            .clipped()
+            .background(Color.gray)
+            .padding(.horizontal, size.width * 0.025)
+            .padding(.vertical, size.height * 0.025)
+        })
+    }
+}
+
+private struct CharacterKey: View {
+    let size: CGSize
+    @ObservedObject var editableDiceKeyState: EditableDiceKeyState
+    var char: Character
+
+    var body: some View {
+        KeyboardKey(size: size, action: { editableDiceKeyState.keyDown(char: char) } , label: {
+            Text("\(String(char))")
+                .font(.system(size: 256, design: .monospaced))
+                .scaledToFit()
+                .minimumScaleFactor(0.01)
+                .lineLimit(1)
+        })
+    }
+}
+
+private struct ImageKey: View {
+    let size: CGSize
+    let action: () -> Void
+    var image: Image
+
+    var body: some View {
+        KeyboardKey(size: size, action: action, label: {
+            image
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+        })
+    }
+}
+
 struct DiceKeyboardView: View {
     @ObservedObject var editableDiceKeyState: EditableDiceKeyState
 
-    var orientationAndNavigationKeys: some View {
-        LazyVGrid(columns: Array(repeating: GridItem(), count: 7), spacing: 4, content: {
-            Text(" ").font(.system(size: 128, design: .monospaced)).scaledToFit().minimumScaleFactor(0.01)
-                .background( Image(systemName: "rotate.left").resizable().aspectRatio(contentMode: .fit) )
-                .onTapGesture { editableDiceKeyState.rotateLeft() }
-            Text(" ").font(.system(size: 128, design: .monospaced)).scaledToFit().minimumScaleFactor(0.01)
-                .background( Image(systemName: "rotate.right").resizable().aspectRatio(contentMode: .fit) )
-                .onTapGesture { editableDiceKeyState.rotateRight() }
-            Text(" ").font(.system(size: 128, design: .monospaced)).scaledToFit().minimumScaleFactor(0.01)
-                .background( Image(systemName: "delete.right.fill").resizable().aspectRatio(contentMode: .fit) )
-                .onTapGesture { editableDiceKeyState.backspace() }
-        })
-    }
-    
     var letterKeys: some View {
-        LazyVGrid(columns: Array(repeating: GridItem(), count: 8), spacing: 4, content: {
+        GeometryReader { geometry in
+        let size = CGSize(width: geometry.size.width / 10, height: geometry.size.width / 20)
+        LazyVGrid(columns: Array(repeating: GridItem(), count: 8), content: {
+//        HStack {
+//            EmptyView()
+//            EmptyView()
+//            EmptyView()
+//            EmptyView()
+            ImageKey(size: size, action: {editableDiceKeyState.rotateLeft()}, image: Image(systemName: "rotate.left"))
+            ImageKey(size: size, action: {editableDiceKeyState.rotateRight()}, image: Image(systemName: "rotate.right"))
+            ImageKey(size: size, action: {editableDiceKeyState.backspace()}, image: Image(systemName: "delete.right.fill"))
+//            EmptyView()
             ForEach(FaceLetters, id: \.self) { faceLetter in
-                Text(faceLetter.rawValue)
-                    .font(.system(size: 128, design: .monospaced)).scaledToFit().minimumScaleFactor(0.01)
-                    .lineLimit(1)
-                    .border(Color.gray, width: 1)
-                    .aspectRatio(1, contentMode: .fit)
-                    .onTapGesture { editableDiceKeyState.enter(letter: faceLetter) }
+                CharacterKey(size: size, editableDiceKeyState: editableDiceKeyState, char: faceLetter.rawValue.first!)
             }
         })
+        }//.aspectRatio(8 /* columns */ / 4 /* rows */, contentMode: .fit)
+        .background(Color.green)
     }
     
     var digitKeys: some View {
-        LazyVGrid(columns: Array(repeating: GridItem(), count: 6), spacing: 4, content: {
+        GeometryReader { geometry in
+            let size = CGSize(width: geometry.size.width / 10, height: geometry.size.height / 5)
+            LazyVGrid(columns: Array(repeating: GridItem(.fixed(geometry.size.width / 10), alignment: .center), count: 8), spacing: geometry.size.width / 100, content: {
+            EmptyView()
+            EmptyView()
+            EmptyView()
+            EmptyView()
+            ImageKey(size: size, action: {editableDiceKeyState.rotateLeft()}, image: Image(systemName: "rotate.left"))
+            ImageKey(size: size, action: {editableDiceKeyState.rotateRight()}, image: Image(systemName: "rotate.right"))
+            ImageKey(size: size, action: {editableDiceKeyState.backspace()}, image: Image(systemName: "delete.right.fill"))
+            EmptyView()
+
             ForEach(FaceDigits, id: \.self) { faceDigit in
-                Text("\(faceDigit.rawValue)")
-                    .font(.system(size: 128, design: .monospaced)).scaledToFit().minimumScaleFactor(0.01)
-                    .lineLimit(1)
-                    .border(Color.gray, width: 1)
-                    .aspectRatio(1, contentMode: .fit)
-                    .onTapGesture { editableDiceKeyState.enter(digit: faceDigit) }
+                CharacterKey(size: size, editableDiceKeyState: editableDiceKeyState, char: faceDigit.rawValue.first!)
             }
         })
+        }.aspectRatio(8 /* columns */ / 4 /* rows */, contentMode: .fit)
     }
     
     var showLetterKeys: Bool {
@@ -56,13 +105,11 @@ struct DiceKeyboardView: View {
 
     var body: some View {
         VStack {
-            orientationAndNavigationKeys
-            Spacer(minLength: 12)
             ZStack(alignment: .top) {
                 letterKeys.showIf(showLetterKeys)
-                digitKeys.hideIf(showLetterKeys)
+                // digitKeys.hideIf(showLetterKeys)
             }
-        }
+        }//.aspectRatio(8 /* columns */ / 4 /* rows */, contentMode: .fit)
 //            .animation(.linear(duration: 0.25))
     }
 }
