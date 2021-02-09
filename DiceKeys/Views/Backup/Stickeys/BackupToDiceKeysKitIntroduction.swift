64d3c1045fb929848693ec31bf0adf8e8694b987
Stuart Schechter <stuart@dicekeys.com>
2021-02-09 20:33:58 +0900

    use WindowDimensions to reuse code

--- DiceKeys/Views/Backup/Stickeys/BackupToDiceKeysKitIntroduction.swift
+++ DiceKeys/Views/Backup/Stickeys/BackupToDiceKeysKitIntroduction.swift
@@ -9,18 +9,13 @@ import SwiftUI
 
 struct BackupToDiceKeysKitIntroduction: View {
     let diceKey: DiceKey
-    #if os(iOS)
-    let shorterSide = UIScreen.main.bounds.size.shorterSide
-    #else
-    let shorterSide = NSScreen.main!.frame.size.height
-    #endif
 
     var body: some View {
         VStack {
             Spacer()
             Instruction("Open your DiceKey kit and take out the box bottom and the 25 dice.")
             Spacer()
-            DiceKeyView(diceKey: diceKey, showDiceAtIndexes: Set()).frame(maxWidth: shorterSide / 6, maxHeight: shorterSide / 2)
+            DiceKeyView(diceKey: diceKey, showDiceAtIndexes: Set()).frame(maxWidth: WindowDimensions.shorterSide / 6, maxHeight: WindowDimensions.shorterSide / 2)
             Spacer()
             Instruction("Next, you will replicate the first DiceKey by copying the arrangement of dice.")
             HStack(alignment: .center, spacing: 0) {
